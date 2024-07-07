2082ca2fb4c33ff73cc9e15eb3d9676b61693e5b
MishGun94 <kolczov9494@gmail.com>
2024-07-07 18:15:11 +0500

    Добавьте файлы проекта.

---
+++ Photographer_agency/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Photographer_agency
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
